eb274209bb3f8aac3007992bbd7e8b0f4fd4660d
paizsvirag <paizs.virag@gmail.com>
2018-05-15 10:59:18 +0200

    added url-fixer.ts

---
+++ week-02/day-02/url-fixer.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+
+url = url.replace('nevertellmethebots', 'nevertellmetheodds');
+url = url.replace('https//','https://');
+
+console.log(url);
