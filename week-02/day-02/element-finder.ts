eb29bd1fd83650cb190743eaed6b161e6bb98939
paizsvirag <paizs.virag@gmail.com>
2018-05-15 16:08:35 +0200

    added element-finder.ts

---
+++ week-02/day-02/element-finder.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// Write a function that checks if the array contains "7"
+// if it contains return "Hoorray" otherwise return "Noooooo"
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 8];
+
+function containsSeven (number) {
+    if(numbers.indexOf(7) === -1) {
+        return 'Noooooo';
+    } else {
+        return 'Hoorraaayy';
+    };
+
+};
+
+
+console.log(containsSeven(numbers));
