0e1953b17afe39cb462771cd0a1a6bf8d30ec36a
paizsvirag <paizs.virag@gmail.com>
2018-05-15 11:32:46 +0200

    takes-longet.ts refactored

--- week-02/day-02/takes-longer.ts
+++ week-02/day-02/takes-longer.ts
@@ -6,7 +6,6 @@ export{};
 // Add "always takes longer than" to between the words "It" and "you"
 let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
 
-quote = quote.slice(0, -18);
-quote = quote.replace('It', 'It always takes longer than ');
+quote = quote.slice(0, -18).replace('It', 'It always takes longer than ');
 
 console.log(quote);
