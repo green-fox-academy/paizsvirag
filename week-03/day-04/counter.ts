91c01727e7c516e232dc3fcdb558d5ff8f309cd8
paizsvirag <paizs.virag@gmail.com>
2018-05-24 10:45:27 +0200

    added counter.ts

--- week-03/day-04/counter.ts
+++ week-03/day-04/counter.ts
@@ -1,11 +1,14 @@
 'use strict';
 
-function factorial(n: number) {
-  let fact = 1;
-  for(var i = 0; i < n; i += 1){
-    fact *= i + 1;
+export{}
+
+function factorial(num: number): number {
+  if (num <= 1) { 
+    return 1;
   }
-  return fact;
+  else {
+    return num * factorial(num - 1);
+  }
 }
 
 console.log('5! is', factorial(5));
