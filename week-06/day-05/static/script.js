3a22a6c8a8762d63354cb59c6f21afe3fe58944c
paizsvirag <paizs.virag@gmail.com>
2018-06-25 17:16:27 +0200

    some minor changes

--- week-06/day-05/static/script.js
+++ week-06/day-05/static/script.js
@@ -3,11 +3,9 @@
 const selected = document.querySelector('#selected'),
       thumbnails = document.querySelectorAll('.thumbnail');
 let pictures = [
-      { path: './pics/asd.jpg', title: 'testTitle', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, laborum. Quis, saepe.' },
-      { path: './pics/asd2.jpg', title: 'testTitle2', desc: 'Minima dignissimos rem laudantium nam ex reprehenderit ducimus accusamus quo cum tempore quae perferendis placeat voluptate, provident modi.' },
-      { path: './pics/cli.png', title: 'testTitle2', desc: 'Minima dignissimos rem laudantium nam ex reprehenderit ducimus accusamus quo cum tempore quae perferendis placeat voluptate, provident modi.' },
-      { path: './pics/ever-other.png', title: 'testTitle2', desc: 'Minima dignissimos rem laudantium nam ex reprehenderit ducimus accusamus quo cum tempore quae perferendis placeat voluptate, provident modi.' },
-      { path: './pics/float-2.gif', title: 'testTitle2', desc: 'Minima dignissimos rem laudantium nam ex reprehenderit ducimus accusamus quo cum tempore quae perferendis placeat voluptate, provident modi.' },
+      { path: './pics/pic1.jpg', title: 'testTitle', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, laborum. Quis, saepe.' },
+      { path: './pics/pic2.jpg', title: 'testTitle2', desc: 'Minima dignissimos rem laudantium nam ex reprehenderit ducimus accusamus quo cum tempore quae perferendis placeat voluptate, provident modi.' },
+      { path: './pics/pic3.jpg', title: 'testTitle2', desc: 'Minima dignissimos rem laudantium nam ex reprehenderit ducimus accusamus quo cum tempore quae perferendis placeat voluptate, provident modi.' }
     ],
     id = 0;
 
