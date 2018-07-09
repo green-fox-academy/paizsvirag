2935fdf30d0d73089edb8e2bdbb5b4d44e50a6e2
paizsvirag <paizs.virag@gmail.com>
2018-07-09 15:10:07 +0200

    added prison exc

---
+++ week-09/prison.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let prison = (name) => {
+  let prisoner = name;
+  let visited = 0;
+  function visit() {
+    if (!prisoner) {
+      console.log(`Nobody is here anymore`);
+    } else {
+      visited++;
+      console.log(`${prisoner} is visited ${visited} time(s)`);
+    };
+  };
+ function escape() {
+   console.log(`BREAKING NEWS, ${prisoner} escaped the prison`);
+   prisoner = null;
+ }
+  return {visit, escape};
+}
+// Create a prison function, that has your name as a private fugutive variable
+// The function should return an object that has two methods:
+//  - visit() // logs "[yourname] is visited [x] time(s)" to the console.
+//    - the [x] times displayes the numbers the function is called
+//    - If the fugitive variable is empty, then display "Nobody is here anymore"
+//  - escape() // logs "BREAKING NEWS, [yourname] escaped the prison" to the console.
+//    - it should empties the fugitive variable
+
+const alcatraz = prison('Sad Panda');
+
+alcatraz.visit(); // Sad Panda is visited 1 time(s)
+alcatraz.visit(); // Sad Panda is visited 2 time(s)
+alcatraz.escape(); // BREAKING NEWS, Sad Panda escaped the prison
+alcatraz.visit(); // Nobody is here anymore
