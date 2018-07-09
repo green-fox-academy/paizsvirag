292b9d8605f0decce89f6bdcaf90564c06f2f699
paizsvirag <paizs.virag@gmail.com>
2018-07-09 14:27:21 +0200

    added the multiplier

---
+++ week-09/multiplier.js
@@ -0,0 +1,19 @@
+// Create the multiplier function that you can use like this:
+
+'use strict';
+
+let multiplier = (number) => {
+  return (multi) => {
+    return number * multi;
+  };
+};
+
+const duplicator = multiplier(2);
+
+console.log(duplicator(5)); // should log 10
+console.log(duplicator(10)); // should log 20
+
+const threeTimes = multiplier(3);
+
+console.log(threeTimes(5)); // should log 15
+console.log(threeTimes(100)); // should log 300
